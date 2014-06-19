3feb31c78b8900043338e5d92f043519c5558d08
ssenpa <soumyajitsen@datacore-611cc1.legacy.data-scan.com>
2014-06-19 13:22:30 -0400

    Initial Commit
    Function bodies created

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ivx <- NULL
+  set <- function(y) {
+    x <<- y
+    ivx <<- NULL
+  }
+  get <- function() x
+  setivx <- function(ivrs) ivx <<- ivrs
+  getivx <- function() ivx
+  list(set = set, get = get,
+       setivx = setivx,
+       getivx = getivx)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ivx <- x$getivx()
+  if(!is.null(ivx)) {
+    message("getting cached data")
+    return(ivx)
+  }
+  data <- x$get()
+  ivx <- solve(data, ...)
+  x$setivx(ivx)
+  ivx
 }
+
+
+
